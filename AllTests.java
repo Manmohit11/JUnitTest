1df1e2e027c640927a29048de38344aacef84264
Manmohit11 <112841444+Manmohit11@users.noreply.github.com>
2022-11-21 09:30:48 +0530

    Create AllTests.java

---
+++ AllTests.java
@@ -0,0 +1,8 @@
+package jUnitTestingPackage;
+
+import org.junit.runner.RunWith;
+@RunWith(Suite.class)
+@SuiteClasses({ testAddNumbers.class, testAddStrings.class })
+public class AllTests {
+  
+}
