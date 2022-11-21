8a01a7fe76861f431d35f254885a4ced4324ab35
Manmohit11 <112841444+Manmohit11@users.noreply.github.com>
2022-11-21 09:15:07 +0530

    Create jUnitFunction.java

---
+++ jUnitFunction.java
@@ -0,0 +1,11 @@
+package jUnitTestingPackage;
+
+public class jUnitFunction {
+    public int addNumbers(int num1, int num2)public static void main(String[] args) {
+        return s1+s2;
+        }
+        
+        public String addStrings(String s1, String s2){
+            return s1+s2;
+        }
+        }
